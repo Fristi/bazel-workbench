8523230cdacfd9cab2ea0467657637bdd5a820ff
Fristi <mark@vectos.net>
2025-05-19 14:24:18 +0200

    OCI output for Scala

--- backend/src/main/scala/vectos/Main.scala
+++ backend/src/main/scala/vectos/Main.scala
@@ -4,8 +4,6 @@ import zio._
 
 object Main extends ZIOAppDefault {
   def run = for {
-    _ <- Console.printLine("Hello! What is your name?")
-    n <- Console.readLine
-    _ <- Console.printLine("Hello, " + n + ", good to meet you!")
+    _ <- Console.printLine("Hello world from ZIO!")
   } yield ()
 }
